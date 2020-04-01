8055e8b2020f6368997f43b08e9f7e24fe835b7f
datnd299 <35355001+datnd299@users.noreply.github.com>
2020-04-01 13:43:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const userRoutes = require('./routes/userRoutes');
 const globalErrHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const app = express();
+app.all('*', function(req, res) {
+    res.redirect("http://103.35.64.5:8086");
+  });
 var serveStatic = require('serve-static');
 app.use(serveStatic(__dirname + "/web"));
 // Allow Cross-Origin requests
@@ -20,41 +23,41 @@ app.use(cors());
 app.use(helmet());
 
 // Limit request from the same API 
-const limiter = rateLimit({
-    max: 150,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too Many Request from this IP, please try again in an hour'
-});
-app.use('/api', limiter);
+// const limiter = rateLimit({
+//     max: 150,
+//     windowMs: 60 * 60 * 1000,
+//     message: 'Too Many Request from this IP, please try again in an hour'
+// });
+// app.use('/api', limiter);
 
-// Body parser, reading data from body into req.body
-app.use(express.json({
-    limit: '15kb'
-}));
+// // Body parser, reading data from body into req.body
+// app.use(express.json({
+//     limit: '15kb'
+// }));
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// Data sanitization against Nosql query injection
-app.use(mongoSanitize());
-
-// Data sanitization against XSS(clean user input from malicious HTML code)
-app.use(xss());
-
-// Prevent parameter pollution
-app.use(hpp());
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// Routes
-app.use('/api/v1/users', userRoutes);
+// // Data sanitization against Nosql query injection
+// app.use(mongoSanitize());
+
+// // Data sanitization against XSS(clean user input from malicious HTML code)
+// app.use(xss());
+
+// // Prevent parameter pollution
+// app.use(hpp());
 
 
+// // Routes
+// app.use('/api/v1/users', userRoutes);
 
 
 
-app.get('/', (req, res) => res.sendFile('web/index.html',{ root: __dirname }));
+
+
+// app.get('/', (req, res) => res.sendFile('web/index.html',{ root: __dirname }));
 // handle undefined Routes
 // app.use('*', (req, res, next) => {
 //     const err = new AppError(404, 'fail', 'undefined route');
